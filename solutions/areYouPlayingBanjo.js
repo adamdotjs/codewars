fe53fbe4feb5e63c11a6e852e2df4d615adc9b1c
Adam Shea <avidworks@live.com>
2022-05-23 23:28:09 -0400

    add banjo solution

---
+++ solutions/areYouPlayingBanjo.js
@@ -0,0 +1,6 @@
+//8 KYU
+// https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+function areYouPlayingBanjo(name) {
+	return name[0] === 'R' || name[0] === 'r' ? `${name} plays banjo` : `${name} does not play banjo`;
+}
