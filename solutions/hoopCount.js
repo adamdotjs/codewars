c362fa5d49e7cc7fe22c7618566193d99d108d05
Adam Shea <avidworks@live.com>
2022-05-31 22:37:30 -0400

    add hoopcount.js

---
+++ solutions/hoopCount.js
@@ -0,0 +1,10 @@
+//8 KYU
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+
+function hoopCount(n) {
+	if (n >= 10) {
+		return 'Great, now move on to tricks';
+	} else {
+		return 'Keep at it until you get it';
+	}
+}
