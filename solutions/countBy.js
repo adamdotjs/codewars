7fcf6b5a8b0fdd8730960b20d8b73800cd51f491
Adam Shea <avidworks@live.com>
2022-06-20 22:32:16 -0400

    add 2 solutions

---
+++ solutions/countBy.js
@@ -0,0 +1,10 @@
+//8 KYU
+//www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+https: function countBy(x, n) {
+	var z = [];
+	for (i = 1; i <= n; i++) {
+		z.push(x * i);
+	}
+	return z;
+}
---
+++ solutions/nameShuffler.js
@@ -0,0 +1,4 @@
+//8 KYU
+// https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
+
+const nameShuffler = (name) => name.split(' ').reverse().join(' ');
